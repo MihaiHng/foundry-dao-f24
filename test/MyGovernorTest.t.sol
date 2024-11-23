b7bc9912b4407485d317b52f47ed25cc9fc8d152
mihai <mihai.hanga@gmail.com>
2024-11-23 12:06:24 +0200

    First Commit

---
+++ test/MyGovernorTest.t.sol
@@ -0,0 +1,103 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Test, console} from "forge-std/Test.sol";
+import {MyGovernor} from "../src/MyGovernor.sol";
+import {Box} from "../src/Box.sol";
+import {Timelock} from "../src/Timelock.sol";
+import {GovToken} from "../src/GovToken.sol";
+
+contract MyGovernorTest is Test {
+    MyGovernor governor;
+    Box box;
+    Timelock timelock;
+    GovToken govToken;
+
+    uint256 public constant MIN_DELAY = 3600; // 1 hour - after a vote passes, you have 1 hour before you can enact
+    uint256 public constant QUORUM_PERCENTAGE = 4; // Need 4% of voters to pass
+    uint256 public constant VOTING_PERIOD = 50400; // This is how long voting lasts
+    uint256 public constant VOTING_DELAY = 7200; // How many blocks till a proposal vote becomes active
+
+    address public USER = makeAddr("user");
+    uint256 public constant INITIAL_SUPPLY = 100 ether;
+
+    address[] proposers;
+    address[] executors;
+    uint256[] values;
+    bytes[] calldatas;
+    address[] targets;
+
+    function setUp() public {
+        govToken = new GovToken();
+        govToken.mint(USER, INITIAL_SUPPLY);
+
+        vm.startPrank(USER);
+        govToken.delegate(USER);
+        timelock = new Timelock(MIN_DELAY, proposers, executors);
+        governor = new MyGovernor(govToken, timelock);
+
+        bytes32 proposerRole = timelock.PROPOSER_ROLE();
+        bytes32 executorRole = timelock.EXECUTOR_ROLE();
+        bytes32 adminRole = timelock.CANCELLER_ROLE();
+
+        timelock.grantRole(proposerRole, address(governor));
+        timelock.grantRole(executorRole, address(0));
+        timelock.revokeRole(adminRole, USER);
+        vm.stopPrank();
+
+        box = new Box();
+        box.transferOwnership(address(timelock));
+    }
+
+    function testCantUpdateBoxWithoutGovernance() public {
+        vm.expectRevert();
+        box.store(1);
+    }
+
+    function testGovernanceUpdatesBox() public {
+        uint256 valueToStore = 888;
+        string memory description = "Store 1 in Box";
+        bytes memory encodedFunctionCall = abi.encodeWithSignature("store(uint256)", valueToStore);
+        calldatas.push(encodedFunctionCall);
+        values.push(0);
+        targets.push(address(box));
+
+        // 1. Propose
+        uint256 proposalId = governor.propose(targets, values, calldatas, description);
+
+        // View the State
+        console.log("Proposal State 1: ", uint256(governor.state(proposalId)));
+        vm.warp(block.timestamp + VOTING_DELAY + 1);
+        vm.roll(block.number + VOTING_DELAY + 1);
+        console.log("Proposal State 2: ", uint256(governor.state(proposalId)));
+
+        // 2. Vote on Proposal
+        string memory reason = "blue frog is cool";
+        uint8 voteWay = 1;
+
+        // Vote Types derived from GovernorCountingSimple:
+        // enum VoteType {
+        //   Against,
+        //   For,
+        //   Abstain
+        //}
+        vm.prank(USER);
+        governor.castVoteWithReason(proposalId, voteWay, reason);
+
+        vm.warp(block.timestamp + VOTING_PERIOD + 1);
+        vm.roll(block.number + VOTING_PERIOD + 1);
+
+        // 3. Queue the Proposal
+        bytes32 descriptionHash = keccak256(abi.encodePacked(description));
+        governor.queue(targets, values, calldatas, descriptionHash);
+
+        vm.warp(block.timestamp + MIN_DELAY + 1);
+        vm.roll(block.number + MIN_DELAY + 1);
+
+        // 4. Execute the Proposal
+        governor.execute(targets, values, calldatas, descriptionHash);
+        console.log("Box Value: ", box.getNumber());
+        assert(box.getNumber() == valueToStore);
+    }
+}
